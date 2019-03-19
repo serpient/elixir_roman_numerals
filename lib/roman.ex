b79b73f12130eb1cef89b9b5851836f480e72e9b
serpient <littlegangwolf@gmail.com>
2019-03-19 15:12:33 -0700

    converts the named numerals into roman with tests

--- lib/roman.ex
+++ lib/roman.ex
@@ -15,4 +15,18 @@ defmodule ROMAN do
   def hello do
     :world
   end
+
+  def roman_numeral(integer) do
+    roman_numeral_map = %{
+      1 => 'I',
+      5 => 'V',
+      10 => 'X',
+      50 => 'L',
+      100 => 'C',
+      500 => 'D',
+      1000 => 'M',
+    }
+    value = Map.fetch(roman_numeral_map, integer)
+    elem(value, 1)
+  end
 end
--- test/roman_test.exs
+++ test/roman_test.exs
@@ -5,4 +5,32 @@ defmodule ROMANTest do
   test "greets the world" do
     assert ROMAN.hello() == :world
   end
+
+  test "converts 1 to roman" do
+    assert ROMAN.roman_numeral(1) == 'I'
+  end
+
+  test "converts 5 to roman" do
+    assert ROMAN.roman_numeral(5) == 'V'
+  end
+
+  test "converts 10 to roman" do
+    assert ROMAN.roman_numeral(10) == 'X'
+  end
+
+  test "converts 50 to roman" do
+    assert ROMAN.roman_numeral(50) == 'L'
+  end
+
+  test "converts 100 to roman" do
+    assert ROMAN.roman_numeral(100) == 'C'
+  end
+
+  test "converts 500 to roman" do
+    assert ROMAN.roman_numeral(500) == 'D'
+  end
+
+  test "converts 1000 to roman" do
+    assert ROMAN.roman_numeral(1000) == 'M'
+  end
 end
