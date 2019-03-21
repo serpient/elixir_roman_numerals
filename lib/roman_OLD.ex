0cae5ac05e76e01efe2d345378f36fba42ee9485
serpient <littlegangwolf@gmail.com>
2019-03-21 13:27:19 -0700

    add more fail attempts to archive

--- lib/roman_OLD.ex
+++ lib/roman_OLD.ex
@@ -18,6 +18,7 @@ defmodule ROMAN_OLD do
       3 => 100,
       4 => 1000
     }
+
     multiples_map[key]
   end
 
@@ -58,7 +59,6 @@ defmodule ROMAN_OLD do
 
   def convert_to_roman_numeral(smallest, num_of_repeated_values, _integer)
       when num_of_repeated_values <= 3.0 do
-
     duplicated_string =
       retrieve_roman_numeral(find_smallest_match(smallest))
       |> String.duplicate(trunc(num_of_repeated_values))
@@ -95,14 +95,15 @@ defmodule ROMAN_OLD do
 
   def split_the_number_into_a_map(integer) do
     length = String.length(Integer.to_string(integer))
+
     Enum.with_index(Integer.digits(integer))
     |> Enum.map(fn {value, index} -> value * multiples(length - index) end)
   end
 
   def convert_split_values_into_roman_string(map_value) do
     Enum.to_list(map_value)
-    |> Enum.map(fn value ->  fetch_roman_symbol(is_exact_match(value), value) end)
-    |> Enum.join
+    |> Enum.map(fn value -> fetch_roman_symbol(is_exact_match(value), value) end)
+    |> Enum.join()
   end
 
   def roman_numeral_converter(integer) do
@@ -110,3 +111,66 @@ defmodule ROMAN_OLD do
     |> convert_split_values_into_roman_string
   end
 end
+
+
+
+
+# defmodule ROMAN do
+#   def get_range(key) do
+#     %{
+#       1 => ["I", "V", "X"],
+#       10 => ["X", "L", "C"],
+#       100 => ["C", "D", "M"]
+#     }
+#     |> Map.fetch!(key)
+#   end
+
+#   def convert_value(value, range) do
+#     base_numeral = Enum.at(range, 0)
+#     middle_numeral = Enum.at(range, 1)
+#     top_numeral = Enum.at(range, 2)
+
+#     cond do
+#       value == 0 ->
+#         ""
+
+#       value == 1 ->
+#         base_numeral
+
+#       value <= 3 ->
+#         String.duplicate(base_numeral, value)
+
+#       value == 4 ->
+#         base_numeral <> middle_numeral
+
+#       value == 5 ->
+#         middle_numeral
+
+#       value <= 8 ->
+#         middle_numeral <> String.duplicate(base_numeral, value - 5)
+
+#       value == 9 ->
+#         base_numeral <> top_numeral
+
+#       value == 10 ->
+#         top_numeral
+#     end
+#   end
+
+#   def range_type(integer) do
+#     cond do
+#       integer <= 10 -> 1
+#       integer <= 100 -> 100
+#       integer > 100 -> 1000
+#     end
+#   end
+
+#   def roman_numeral_converter(integer) do
+#     length = String.length(Integer.to_string(integer))
+
+#     Enum.with_index(Integer.digits(integer))
+#     |> Enum.map(fn {value, index} ->
+#       convert_value(value * range_type(length - index), range_type(value))
+#     end)
+#   end
+# end
