381c2e1efc6ba2f90694adf96062f090e3dc484c
serpient <littlegangwolf@gmail.com>
2019-03-20 17:43:26 -0700

    initial roman numeral attempt using TDD

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,32 +1,77 @@
 defmodule ROMAN do
-  @moduledoc """
-  Documentation for ROMAN.
-  """
+  def roman_numeral_map() do
+    %{
+      1 => "I",
+      5 => "V",
+      10 => "X",
+      50 => "L",
+      100 => "C",
+      500 => "D",
+      1000 => "M"
+    }
+  end
 
-  @doc """
-  Hello world.
+  def find_smallest_match(integer) when integer == 1 do
+    1
+  end
 
-  ## Examples
+  def find_smallest_match(integer) do
+    Map.keys(roman_numeral_map())
+    |> Enum.reverse()
+    |> Enum.find(fn x -> x < integer end)
+  end
 
-      iex> ROMAN.hello()
-      :world
+  def find_next_largest_match(integer) do
+    Map.keys(roman_numeral_map())
+    |> Enum.find(fn x -> x > integer end)
+  end
 
-  """
-  def hello do
-    :world
+  def convert_to_roman_numeral(smallest, num_of_repeated_values)
+      when num_of_repeated_values > 3.0 do
+    next_largest_roman_numeral =
+      Map.fetch!(roman_numeral_map(), find_next_largest_match(smallest))
+
+    [next_largest_roman_numeral]
+    |> List.insert_at(0, Map.fetch!(roman_numeral_map(), smallest))
+    |> List.to_string()
+  end
+
+  def convert_to_roman_numeral(smallest, num_of_repeated_values)
+      when num_of_repeated_values <= 3.0 and smallest == 1 do
+    Map.fetch!(roman_numeral_map(), smallest)
+    |> String.duplicate(trunc(num_of_repeated_values))
+  end
+
+  def convert_to_roman_numeral(smallest, num_of_repeated_values)
+      when num_of_repeated_values <= 3.0 do
+    string_lower_than_smallest = Map.fetch!(roman_numeral_map(), find_smallest_match(smallest))
+
+    duplicated_string =
+      String.duplicate(string_lower_than_smallest, trunc(num_of_repeated_values))
+
+    [Map.fetch!(roman_numeral_map(), smallest)]
+    |> List.insert_at(1, duplicated_string)
+    |> List.to_string()
+  end
+
+  def fetch_roman_symbol(val, integer) when val == :error do
+    smallest_matching_roman_numeral = find_smallest_match(integer)
+
+    num_of_repeated_values =
+      if(smallest_matching_roman_numeral == 1,
+        do: integer - 0,
+        else: integer - smallest_matching_roman_numeral
+      )
+
+    convert_to_roman_numeral(smallest_matching_roman_numeral, num_of_repeated_values)
+  end
+
+  def fetch_roman_symbol(val, _) when elem(val, 0) == :ok do
+    elem(val, 1)
   end
 
   def roman_numeral(integer) do
-    roman_numeral_map = %{
-      1 => 'I',
-      5 => 'V',
-      10 => 'X',
-      50 => 'L',
-      100 => 'C',
-      500 => 'D',
-      1000 => 'M',
-    }
-    value = Map.fetch(roman_numeral_map, integer)
-    elem(value, 1)
+    Map.fetch(roman_numeral_map(), integer)
+    |> fetch_roman_symbol(integer)
   end
 end
