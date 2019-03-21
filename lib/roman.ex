d30112556398f5d8b7593db050bf9f920d17a4a1
serpient <littlegangwolf@gmail.com>
2019-03-21 11:06:29 -0700

    add additional code to handle up to 49

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,8 +1,14 @@
 defmodule ROMAN do
   def roman_numeral_converter(integer) do
     cond do
-      integer == 1 -> String.duplicate("I", integer)
-      integer == 2 -> String.duplicate("I", integer)
+      integer == 1 -> "I"
+      integer <= 3 -> String.duplicate("I", integer)
+      integer == 4 -> "IV"
+      integer == 5 -> "V"
+      integer <= 8 -> "V" <> String.duplicate("I", integer - 5)
+      integer == 9 -> "IX"
+      integer == 10 -> "X"
+      integer <= 49 -> "X" <> roman_numeral_converter(integer - 10)
     end
   end
 end
